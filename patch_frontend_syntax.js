6e42b7c6f99f689ffa25e7cc338730096039dd14
meherab2151 <mehrabhossain2151@gmail.com>
2026-09-26 21:31:04 +0600

    chore: cleanup old scripts

--- patch_frontend_syntax.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('frontend/src/pages/Deliveries.jsx', 'utf8');
-content = content.replace(/\\`/g, '`');
-content = content.replace(/\\\$/g, '$');
-fs.writeFileSync('frontend/src/pages/Deliveries.jsx', content);
-console.log('patched frontend Deliveries.jsx');
--- patch_syntax.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('backend/models/shipmentModel.js', 'utf8');
-content = content.replace(/\\`/g, '`');
-content = content.replace(/\\\$/g, '$');
-fs.writeFileSync('backend/models/shipmentModel.js', content);
-console.log('patched');
